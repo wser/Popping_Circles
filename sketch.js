eca1bbe1fec97dd46fadb4a7a41dec7dc282daa7
wser <w.web.dev@gmail.com>
2022-07-05 08:59:54 +0200

    added q5

--- sketch.js
+++ sketch.js
@@ -1,6 +1,9 @@
+new Q5('global');
+
 let circleFilling = false;
 let circleSize = 0;
-let circleColor;
+let totalArea = 0;
+let circleColor, area;
 
 const circles = [];
 
@@ -14,44 +17,50 @@ function draw() {
   background(150);
 
   if (circleFilling) {
-    circleSize += 2;
+    circleSize += 1;
 
     const overlappingCircle = getOverlappingCircle();
-    if(overlappingCircle){
+    if (overlappingCircle) {
       circleFilling = false;
-      circles.splice(circles.indexOf(overlappingCircle), 1); 
+      circles.splice(circles.indexOf(overlappingCircle), 1);
     }
-    
+
     if (isOffScreen()) {
       circleFilling = false;
     }
 
     fill(circleColor);
     circle(mouseX, mouseY, circleSize);
-  }
 
+    area = (circleSize / 2) ** 2 * PI;
+    totalArea = totalArea + area;
+  }
 
   for (const c of circles) {
     c.draw();
   }
+
+  fill(50);
+  text('Total area: ' + round(totalArea) + 'pixels', 20, 20);
 }
 
 function getOverlappingCircle() {
-  for(const c of circles){
-    if(dist(c.x, c.y, mouseX, mouseY) <
-      circleSize / 2 + c.size / 2 + 2){
+  for (const c of circles) {
+    if (dist(c.x, c.y, mouseX, mouseY) < circleSize / 2 + c.size / 2 + 2) {
       return c;
     }
   }
-  
+
   return undefined;
 }
 
-function isOffScreen(){
-  return mouseX < circleSize / 2 ||
-         mouseX > width - circleSize / 2 ||
-         mouseY < circleSize / 2 ||
-         mouseY > height - circleSize / 2;
+function isOffScreen() {
+  return (
+    mouseX < circleSize / 2 ||
+    mouseX > width - circleSize / 2 ||
+    mouseY < circleSize / 2 ||
+    mouseY > height - circleSize / 2
+  );
 }
 
 function mousePressed() {
@@ -68,7 +77,6 @@ function mouseReleased() {
 }
 
 class Circle {
-
   constructor(x, y, size, color) {
     this.x = x;
     this.y = y;
